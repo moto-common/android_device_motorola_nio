298cc4279e0e13998900d038221f8b4ced92e188
electimon <electimon@gmail.com>
2023-05-09 11:30:20 +0000

    borneo: Use xhdpi instead of xxhdpi
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I96a5c10bd9ff1cc21a80cb694a78ec65945f454e

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ DEVICE_PATH := device/motorola/borneo/rootdir
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.sf.lcd_density=280
