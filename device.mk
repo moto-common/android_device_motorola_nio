e8ca72dfa3c7890e9d17676e3adc81596b55faa3
electimon <electimon@gmail.com>
2023-05-11 17:37:42 +0000

    borneo: Set PRODUCT_MODEL
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I5c4b6d5fbffb045b02d1b325c647120471290656

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ PRODUCT_PACKAGES += \
 TARGET_USES_EGISTEC_FINGERPRINT := true
 TARGET_USES_FPC_FINGERPRINT := true
 
+# Model
+PRODUCT_MODEL := moto g power 2021
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/sm4250-common/platform.mk)
 
